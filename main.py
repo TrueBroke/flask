307dce4267a71eaec51d4a914c9a905d643f616d
YourSempay <BRAWLWIN123@mail.ru>
2026-03-20 15:23:09 +1000

    сделал маршрутизацию и еще стилей

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return '<h1>О нас</h1>'
+
+@app.route('/contact')
+def contact():
+    return '<h1>Наши контакты</h1>'
+
+@app.route('/user')
+@app.route('/profile')
+def user_profile():
+    return '<h1>Профиль пользователя</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'<h1>Профиль пользователя: {username}</h1>'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'<h1>Пост №{post_id}</h1>'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'<h1>Подпуть: {subpath}</h1>'
+
 class User:
     def __init__(self, id, name, email):
         self.id = id
